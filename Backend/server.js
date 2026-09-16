fe22a616168348981a50a1732b4e2f39ddf07fdb
Jatin <jy799914@gmail.com>
2026-09-16 14:33:43 +0530

    modified Server.js (Jatin)

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,40 @@
-import dns from "node:dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 import express from "express";
 import mongoose from "mongoose";
-import routerStaff from "./routes/staff.js";
-import routerAttendance from "./routes/attendance.js";
-import "dotenv/config";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./Routes/auth.js";
+import service from "./Routes/service.js";
+import bookingRouter from "./Routes/Booking.js";
+
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
 
+app.use(cors());
 app.use(express.json());
 
-async function mongodb() {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        console.log("connected to Database");
-    } catch (err) {
-        console.log(err);
-    }
-}
-mongodb();
-
-
-app.use("/api/staff", routerStaff);
-app.use("/api/attendance", routerAttendance);
-
-app.listen(port, () => {
-    console.log("server is listening on port", port);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Booking OS API running",
+  });
 });
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.use("/api/auth", authRoutes);
+    app.use("/api/service", service);
+    app.use("/api/booking", bookingRouter);
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Error:", error);
+  });
